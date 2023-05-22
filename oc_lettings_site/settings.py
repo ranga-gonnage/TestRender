6c65c1dc72fd6acd52d5ef39e7f70847ffc1af21
Ranga Gonnage <rangagonnage@mbp-de-ranga.home>
2023-05-22 20:54:54 +0200

    Add host

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['oc-lettings-jmd-3.herokuapp.com']
+ALLOWED_HOSTS = ['oc-lettings-jmd-3.herokuapp.com', 'testrender-q2i0.onrender.com']
 
 
 # Application definition
